737ec66ff4d166d9db7f2962e930457f72a83a2d
doni-robert <irobertndungu@gmail.com>
2023-06-21 20:00:27 +0300

    update

--- console.py
+++ console.py
@@ -272,7 +272,7 @@ class HBNBCommand(cmd.Cmd):
         print("[Usage]: all <className>\n")
 
     def do_count(self, args):
-        """ Count current number of class instances"""
+        """ Count current number of class instances """
         count = 0
         for k, v in storage.all().items():
             if args == k.split('.')[0]:
